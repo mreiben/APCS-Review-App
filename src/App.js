540ca6d51e678e2041b3854798da733c3d93d3c2
mreiben <jason.eiben@gmail.com>
2017-06-24 14:02:37 -0400

    initial commit with auth0 starter

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { Navbar, Button } from 'react-bootstrap';
+import './App.css';
+
+class App extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth;
+
+    return (
+      <div>
+        <Navbar fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">AP Computer Science Review</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <ul className="nav navbar-nav navbar-right">
+            <Button
+              bsStyle="primary"
+              className="btn-margin"
+              onClick={this.goTo.bind(this, 'home')}
+            >
+              Home
+            </Button>
+            {
+              !isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin last-btn"
+                    onClick={this.login.bind(this)}
+                  >
+                    Log In
+                  </Button>
+                )
+            }
+            {
+              isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.goTo.bind(this, 'profile')}
+                  >
+                    Profile
+                  </Button>
+                )
+            }
+            {
+              isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin last-btn"
+                    onClick={this.logout.bind(this)}
+                  >
+                    Log Out
+                  </Button>
+                )
+            }
+          </ul>
+        </Navbar>
+        <div className="container">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'jeiben.auth0.com',
+  clientId: 'EiA6QEyaDDwiobOUyDxfiPg8hODuWZMz',
+  callbackUrl: 'http://localhost:3000/callback'
+}
